d6dfb97da66dc3eabeb4c1145471e7b5697a6d76
Jaynil1611 <48921037+Jaynil1611@users.noreply.github.com>
2021-05-24 16:53:41 +0530

    refactor: removed logs

--- backend/index.js
+++ backend/index.js
@@ -22,9 +22,6 @@ const likedVideoRouter = require("./routes/likedVideo.router");
 const savedVideoRouter = require("./routes/savedVideo.rouer");
 
 initializeDBConnection();
-// storeVideos();
-// createUser();
-// userId: 60a35a72ffb1fa01498940eb
 
 app.use("/videos", videoRouter);
 app.use("/user/:userId/history", historyRouter);
@@ -45,7 +42,7 @@ app.use((err, req, res, next) => {
   res.status(500).json({ success: false, message: "error occured, see the error message key for more details", errorMessage: err.message })
 })
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log('server started on port: ', PORT);
