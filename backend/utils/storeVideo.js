b2e694fc4f7d3e5316f053ac91e738926509767b
Jaynil1611 <48921037+Jaynil1611@users.noreply.github.com>
2021-05-24 18:14:54 +0530

    refactor: updated video model & moved video data to utils

--- backend/utils/storeVideo.js
+++ backend/utils/storeVideo.js
@@ -1,16 +1,15 @@
-const { Video } = require('../models/video.model');
-const { videoData } = require("../models/video.data");
+const { Video } = require("../models/video.model");
+const { videoData } = require("./video.data");
 
 async function storeVideos() {
   try {
     videoData.forEach(async (video) => {
       const videoToBeSaved = new Video(video);
       const savedVideo = await videoToBeSaved.save();
-    })
+    });
   } catch (error) {
     console.log("Error saving videos to db!");
   }
 }
 
-
-module.exports = { storeVideos }
+module.exports = { storeVideos };
